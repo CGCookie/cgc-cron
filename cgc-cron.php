597edca5c2b62fd3228db0b1feabbdbc1e82b6b8
Pippin Williamson <pippin@pippinsplugins.com>
2014-03-04 10:09:13 -0600

    Add first file

---
+++ cgc-cron.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name: CGC Cron
+ * Description: Custom cron events and control for CGC
+ * Author: Pippin Williamson
+ * Version: 1.0
+ */
+
+class CGC_Cron {
+	
+	
+
+}
+new CGC_Cron;
